752916d2c378e0e0d57abfab6bdc3ccf3aa454f7
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-12-06 10:49:49 +0100

    start app

--- App.tsx
+++ App.tsx
@@ -1,21 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, TouchableOpacity, Text, View } from "react-native";
+import Constants from "expo-constants";
+import { Button, Colors, Headline, TextInput } from "react-native-paper";
 
-export default function App() {
+const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isVisble, setIsVisible] = useState(true);
+
+  function handleLogin() {
+    console.log("login");
+  }
+
+  function navigateToTerms() {
+    // navigation.navigate(AppRoutes.TERMS_SCREEN);
+  }
+
+  function toggleSecureIcon() {
+    setIsVisible(!isVisble);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View style={styles.header}>
+        <Headline style={styles.headerText}>SpaceCraft</Headline>
+      </View>
+      <View style={styles.content}>
+        <TextInput
+          label="Email"
+          value={email}
+          onChangeText={(value) => setEmail(value)}
+        />
+        <TextInput
+          label="Password"
+          value={password}
+          secureTextEntry={isVisble}
+          onChangeText={(value) => setPassword(value)}
+          right={
+            <TextInput.Icon
+              onPress={toggleSecureIcon}
+              name={isVisble ? "eye-off" : "eye"}
+            />
+          }
+        />
+        <Button
+          onPress={handleLogin}
+          style={styles.submitButton}
+          mode="contained"
+        >
+          Login
+        </Button>
+        <TouchableOpacity onPress={navigateToTerms}>
+          <Text style={styles.tocText}>
+            by login you accept the Terms and Conditions.
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  content: {
+    padding: 16,
+  },
+  header: {
+    paddingTop: Constants.statusBarHeight,
+    alignItems: "center",
+    justifyContent: "center",
+    height: 256,
+    backgroundColor: Colors.purple500,
+  },
+  headerText: {
+    color: Colors.white,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+  },
+  submitButton: {
+    marginVertical: 32,
+    marginHorizontal: 16,
+    backgroundColor: Colors.purple500,
+  },
+  tocText: {
+    textAlign: "center",
+    fontSize: 14,
+    color: Colors.grey500,
   },
 });
+
+export default LoginScreen;
