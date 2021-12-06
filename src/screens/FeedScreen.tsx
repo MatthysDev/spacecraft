79bf6e5de534e6a8c9d1981875c2d2b7dd714ae4
Matthys Ducrocq <Ducrocq.matthys@gmail.com>
2021-12-06 14:44:09 +0100

    add flatlist tgat work

--- src/screens/FeedScreen.tsx
+++ src/screens/FeedScreen.tsx
@@ -7,12 +7,20 @@ import {
   View,
   FlatList,
 } from "react-native";
+import { Card, Paragraph, Title } from "react-native-paper";
 
 import { data } from "../../api/data";
 
+interface propsCard {
+  name: string;
+  model: string;
+  crew: string;
+  hyperdrive_rating: string;
+  cost_in_credits: string;
+}
 const FeedScreen = () => {
   const renderItem = ({ item }: any) => (
-    <Item
+    <FeedCard
       name={item.name}
       model={item.model}
       crew={item.crew}
@@ -20,20 +28,23 @@ const FeedScreen = () => {
       cost_in_credits={item.cost_in_credits}
     />
   );
-  const Item = (
-    { name }: any,
-    { model }: any,
-    { crew }: any,
-    { hyperdrive_rating }: any,
-    { cost_in_credits }: any
-  ) => (
-    <View>
-      <Text>{name}</Text>
-      <Text>{model}</Text>
-      <Text>{crew}</Text>
-      <Text>{hyperdrive_rating}</Text>
-      <Text>{cost_in_credits}</Text>
-    </View>
+  const FeedCard = ({
+    name,
+    model,
+    crew,
+    hyperdrive_rating,
+    cost_in_credits,
+  }: propsCard) => (
+    <Card style={styles.card}>
+      <Card.Title title={name} />
+      <Card.Content>
+        <Title style={styles.cardTitle}>{model}</Title>
+
+        <Paragraph>{crew}</Paragraph>
+        <Paragraph>{hyperdrive_rating}</Paragraph>
+        <Paragraph>{cost_in_credits}</Paragraph>
+      </Card.Content>
+    </Card>
   );
   return (
     <SafeAreaView style={styles.safeContainer}>
@@ -52,10 +63,18 @@ const styles = StyleSheet.create({
   safeContainer: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
+    backgroundColor: "#000000",
   },
   container: {
     paddingHorizontal: 20,
     marginTop: 20,
+  },
+  card: {
+    margin: 5,
+    padding: 5,
+  },
+  cardTitle: {
+    fontSize: 16,
   },
 });
 
